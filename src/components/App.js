c83af43a6d88e1506ddb44bff5d8eac9d90f5653
Lukasz Sobek <lukaszsobek@me.com>
2018-02-18 18:16:43 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../assets/styles/App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <p>App works</p>
+    );
+  }
+}
