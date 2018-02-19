33d8dfe62b0fd604dd5fdf7a0628c6e60cae983d
Lukasz Sobek <lukaszsobek@me.com>
2018-02-19 10:37:09 +0100

    Refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,30 @@
-import cartContent from "./cartContent";
 
-export default function() {
-    return {
-        cartContent: cartContent()
-    };
-}
+const initialState = {
+    cartContent: [
+        {
+            name: "Product1",
+            comments: "",
+            net_price: 8.00,
+            tax: 19
+        },{
+            name: "Product2",
+            comments: "",
+            net_price: 11.00,
+            tax: 19
+        },{
+            name: "Product3",
+            comments: "",
+            net_price: 16.25,
+            tax: 7
+        },{
+            name: "Product4",
+            comments: "",
+            net_price: 12.00,
+            tax: 7
+        },
+    ] 
+};
+
+const rootReducer = (state = initialState, action) => state;
+
+export default rootReducer;
